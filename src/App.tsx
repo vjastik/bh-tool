fb37d2edde8a7bdb8308fb8907904969ea2d26f3
vjastik <vjastik@gmail.com>
2024-12-03 16:00:36 +0200

    Changed routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { store } from './store/store';
-import CharacterList from './components/character/list/CharacterList';
-import CharacterForm from './components/character/form/CharacterForm';
-import CharacterDetails from './components/CharacterDetails';
-import LanguageSelector from './components/LanguageSelector';
+import React from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { store } from "./store/store";
+import CharacterList from "./components/character/list/CharacterList";
+import CharacterForm from "./components/character/form/CharacterForm";
+import CharacterDetails from "./components/CharacterDetails";
+import LanguageSelector from "./components/LanguageSelector";
+
+const GH_PAGES_PATH = "/bh-tool";
 
 export default function App() {
   return (
@@ -13,9 +15,15 @@ export default function App() {
       <Router>
         <LanguageSelector />
         <Routes>
-          <Route path="/" element={<CharacterList />} />
-          <Route path="/character/new" element={<CharacterForm />} />
-          <Route path="/character/:id" element={<CharacterDetails />} />
+          <Route path={`${GH_PAGES_PATH}/`} element={<CharacterList />} />
+          <Route
+            path={`${GH_PAGES_PATH}/character/new`}
+            element={<CharacterForm />}
+          />
+          <Route
+            path={`${GH_PAGES_PATH}/character/:id`}
+            element={<CharacterDetails />}
+          />
         </Routes>
       </Router>
     </Provider>
