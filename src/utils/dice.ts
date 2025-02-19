675b8a90caab4646e6bdffdc8db953f1a894240b
vjastik <vjastik@gmail.com>
2025-02-19 17:00:47 +0200

    Changed dice value math

--- src/utils/dice.ts
+++ src/utils/dice.ts
@@ -3,7 +3,7 @@ import { DiceType } from '../types/character';
 export const DICE_VALUES: DiceType[] = ['d4', 'd6', 'd8', 'd10', 'd12'];
 
 export const getDiceValue = (dice: DiceType): number => {
-  return parseInt(dice.substring(1));
+  return parseInt(dice.substring(1)) / 2;
 };
 
 export const getDiceIndex = (dice: DiceType): number => {
